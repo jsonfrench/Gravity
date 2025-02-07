122aaf47c8d5a64a99cf240f8650fc470b01ab00
Jason <114097230+jsonfrench@users.noreply.github.com>
2025-02-07 00:34:05 -0500

    Fourier Graph Comparison
    Plotted the difference in a planet's orbit with and without a second body acting on it.
    Added a multi-graph view.
    Reduced plot domain for better visualization of data.

--- orbit.py
+++ orbit.py
@@ -2,40 +2,21 @@
 
 import numpy as np
 from scipy.integrate import odeint
-import scipy as sci
+from scipy.fft import fft
+from scipy.fft import rfft
 import matplotlib.pyplot as plt
-import statistics as stat
 
 
+# Constatnts
+G = 6.674e-11
 
-# Constants
-G = 1
+# Simulation setup
+samples_per_time = 10
+time_steps = 100000 * 1
+t = np.linspace(0, time_steps, time_steps*samples_per_time) 
+freq = np.fft.rfftfreq(len(t), 1/samples_per_time) * 10
 
-# Parameters
-m0 = 1000000     # Large mass (sun)
-m1 = 1          # small mass (planet)
-m2 = 1
-
-# Initial Values
-x0 = 0         
-y0 = 0         
-x1 = 1        
-y1 = 0      
-x2 = 2
-y2 = 0 
-vx0 = 0        
-vy0 = 0        
-vx1 = 0        
-#vy1 = np.sqrt(G*m1) * 0.9         # eliptical
-#vy1 = np.sqrt(G*m1) * 1         # circular
-vy1 = np.sqrt(G*m1) * 1        # eliptical
-#vy1 = np.sqrt(G*m1) * np.sqrt(2)    # parabolic 
-# vy1 = np.sqrt(G*m1) * np.sqrt(2) * 1.1   # hyperbolic
-vx2 = 0
-vy2 = np.sqrt(G*m1) * 1.1         # eliptical
-
-
-def dSdt(IVP, t, G, M): 
+def one_body(IVP, t, G, M): 
     
     x, y, vx, vy = IVP
 
@@ -46,47 +27,82 @@ def dSdt(IVP, t, G, M):
 
     return [vx, vy, ax, ay]
 
-t_max = 10
-samples_per_time = 50000
+def two_body(IVP, t, G, M, m1, m2): 
+    
+    x1, y1, vx1, vy1, x2, y2, vx2, vy2 = IVP
 
-t = np.linspace(0, t_max, samples_per_time*t_max)
+    r1 = np.sqrt(x1**2 + y1**2) # distance of 1st body to sun
+    r2 = np.sqrt(x2**2 + y2**2) # distance of 2nd body to sun
+    d = np.sqrt((x1-x2)**2 + (y1-y2)**2)    # distance between 1st and 2nd body
 
-solution = odeint(dSdt, [x2, y2, vx2, vy2], t, args=(G, m1))
+    ax1 = (-G * M * x1 / r1**3) + (-G * m2 * (x2-x1) / d**3)
+    ay1 = (-G * M * y1 / r1**3) + (-G * m2 * (y2-y1) / d**3)
+    ax2 = (-G * M * x2 / r2**3) + (-G * m1 * (x1-x2) / d**3)
+    ay2 = (-G * M * y2 / r2**3) + (-G * m1 * (y1-y2) / d**3)
 
-x = solution[:, 0]     # select all x values
-y = solution[:, 1]     # select all y values
+    return [vx1, vy1, ax1, ay1, vx2, vy2, ax2, ay2]
 
-c = np.cos(200*2*np.pi*t)
+def plot_two_body_orbit(solution):
 
-plt.figure(figsize=(12,6))
+    two_body_solution = solution
 
-plt.subplot(1,2,1)
-plt.plot(x,y)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('orbital position')
+    two_body_x1 = two_body_solution[:, 0]
+    two_body_y1 = two_body_solution[:, 1]
+    two_body_x2 = two_body_solution[:, 4]
+    two_body_y2 = two_body_solution[:, 5]
 
-array = np.zeros(len(x))
-for i in range(len(x)-1):
-    array[i] = (t[i+1]-t[i])*(x[i+1]+x[i])
+    plt.plot(two_body_x1,two_body_y1)
+    plt.plot(two_body_x2,two_body_y2)
+    plt.show()
 
-x_avg = (1/2*t_max)*np.sum(array)
+def plot_fourier(solutions):
 
-x = x - x_avg
-x_hat = np.fft.rfft(x)
-freq = np.fft.rfftfreq(len(t), 1/samples_per_time)
+    for solution in solutions:
+        values = solution[:, 0]
+        fourier = np.fft.rfft(values)
+        plt.plot(freq,np.abs(fourier))
+    plt.show()
 
-F_upper = 10000
-height = 200
+def plot_fourier_difference(solution_1, solution_2):
 
-peaks, properties = sci.signal.find_peaks(np.abs(x_hat[:F_upper]), height = height)
+    solution_1_values = solution_1[:, 0]
+    solution_2_values = solution_2[:, 0]
 
-plt.subplot(1,2,2)
-plt.plot(freq[:F_upper],np.abs(x_hat[:F_upper]))
-plt.scatter(freq[peaks], np.abs(x_hat[peaks]), c='orange')
-plt.text(x=250, y=100000, s=f'peak freq = {freq[peaks]}')
-plt.xlabel('frequency')
-plt.ylabel('magnitude')
-plt.title('orbital x coordinate in frequency domain')
+    solution_1_fourier = np.fft.rfft(solution_1_values)
+    solution_2_fourier = np.fft.rfft(solution_2_values)
+
+    max_frequency = int(0.02 * time_steps / samples_per_time)
+    truncated_range = freq[:max_frequency]
+
+    plt.plot(truncated_range,np.abs(solution_1_fourier)[:max_frequency], color="blue", label="theoretical orbit")
+    plt.plot(truncated_range,np.abs(solution_2_fourier)[:max_frequency], color="orange", label="actual orbit")
+    # plt.plot(truncated_range,np.abs(solution_1_fourier - solution_2_fourier), color="red")
+    plt.plot(truncated_range,(solution_1_fourier - solution_2_fourier)[:max_frequency], color="red", label="real difference")
+    plt.plot(truncated_range,(solution_1_fourier - solution_2_fourier)[:max_frequency] * -1j, color="black", label="imaginary difference")
+    plt.title("Difference in theoretical and observed")
+    plt.legend()
+    # plt.show()
+
+examples = [
+    # [1, 0, 0, np.sqrt(G*100000/1), 1.01, 0, 0, np.sqrt(G*100000/1.01), 100000, 1, 1],   # coil orbit
+    # [1, 0, 0, np.sqrt(G*100000/1), 1.01, 0, 0, np.sqrt(G*100000/1.01) * 0.99, 100000, 1, 1], # messier coild orbit
+    # [1, 0, 0, np.sqrt(G*10000/1), 1.01, 0, 0, np.sqrt(G*100000/1.01), 100000, 1, 100], # massive 2nd body
+    [1, 0, 0.001, np.sqrt(G*100000/1), 2, 0, 0, np.sqrt(G*100000/2), 100000, 1, 1], # two bodies, minor x velocity 
+    [1, 0, 0.001, np.sqrt(G*100000/1), 2, 0, 0, np.sqrt(G*100000/2), 100000, 1, 2], 
+    [1, 0, 0.001, np.sqrt(G*100000/1), 2, 0, 0, np.sqrt(G*100000/2), 100000, 1, 5],
+    # [1, 0, 0, np.sqrt(G*100000/1), 1.5, 0, 0, np.sqrt(G*100000/2), 100000, 1000, 1], # two bodies, massive 1st body
+    # [1, 0, 0, np.sqrt(G*100000/1), 1.1, 0, 0, np.sqrt(G*100000/1.1), 100000, 100, 1], # two bodies, (less) massive 1st body 
+    # [1, 0, 0, np.sqrt(G*100000/1), -1.01, 0, 0, np.sqrt(G*100000/1.01), 10000, 1, 1], # two bodies travelling opposite directions
+]
+
+fig = plt.figure(figsize=(8,6))
+
+for i in range(len(examples)): 
+    plt.subplot(len(examples), 1, i+1)
+    x1, y1, vx1, vy1, x2, y2, vx2, vy2, m0, m1, m2 = examples[i]
+    two_body_solution = odeint(two_body, [x1, y1, vx1, vy1, x2, y2, vx2, vy2], t, args=(G, m0, m1, m2))
+    one_body_solution = odeint(one_body, [x1, y1, vx1, vy1], t, args=(G, m0))
+    # plot_two_body_orbit(two_body_solution)
+    plot_fourier_difference(one_body_solution, two_body_solution) 
 plt.tight_layout()
 plt.show()
